06ed2b6ad350772bdd1fdc7f71b8ad609a4e75c7
Sheveliow Igor <strelanedura@list.ru>
2023-07-28 13:41:05 +0300

    Add hometasks

---
+++ hometask_1.1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и
+// на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int Prompt (int num)
+{
+    if (num <= 0) num = num*-1;
+    int y = num/10;
+    int z = y%10;
+        if (99 < num && num < 999)
+        {
+        Console.WriteLine(z);
+        }
+        else
+        {
+            Console.WriteLine("Число не является трёхзначным");
+        }
+    return z;
+}
+
+Console.Clear();
+Console.WriteLine("Введите трёхзначное число ");
+int number = int.Parse(Console.ReadLine()!);
+int result = Prompt (number);
---
+++ hometask_3.3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели,
+// и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+int Prompt (int num)
+{
+    if (0 < num && num < 8)
+        {
+        if (0 < num && num < 6) Console.WriteLine("Нет");
+        else Console.WriteLine("Да");
+        }
+    else Console.WriteLine("Цифра не соответствует дням недели");
+    return num;
+}
+
+Console.Clear();
+Console.WriteLine("Введите число ");
+int x = int.Parse(Console.ReadLine()!);
+int result = Prompt(x);
